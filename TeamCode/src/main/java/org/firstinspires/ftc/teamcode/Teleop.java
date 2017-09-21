bbb74335af22af1baaf10df0856f403007a04e9c
robotics <enzotechnerd@gmail.com>
2017-09-21 18:59:46 -0500

    RC01

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by robotics on 9/19/2017.
+ */
+
+public class Teleop {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by robotics on 9/19/2017.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous(name = "Cool Program", group = "Testing Program")
+public class test {
+
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
